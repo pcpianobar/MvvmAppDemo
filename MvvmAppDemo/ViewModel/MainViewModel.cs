0c91470485fd4f60df39bd1d67cc3024799eafbb
Kihun.Jung <pcpianobar@gmail.com>
2017-02-03 07:11:04 +0900

    시작

---
+++ MvvmAppDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+using Xamarin.Forms;
+
+namespace MvvmAppDemo.ViewModel
+{
+	/// <summary>
+	/// This class contains properties that the main View can data bind to.
+	/// <para>
+	/// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+	/// </para>
+	/// <para>
+	/// You can also use Blend to data bind with the tool's support.
+	/// </para>
+	/// <para>
+	/// See http://www.galasoft.ch/mvvm
+	/// </para>
+	/// </summary>
+	public class MainViewModel : ViewModelBase
+	{
+		private readonly INavigationService _navigationService;
+		public ICommand NavigationCommand { get; private set; }
+
+		/// <summary>
+		/// Initializes a new instance of the MainViewModel class.
+		/// </summary>
+		public MainViewModel (INavigationService navigationService)
+		{
+			////if (IsInDesignMode)
+			////{
+			////    // Code runs in Blend --> create design time data.
+			////}
+			////else
+			////{
+			////    // Code runs "for real"
+			////}
+
+			_navigationService = navigationService;
+			NavigationCommand = new Command (() => Navigation ());
+		}
+
+		private void Navigation ()
+		{
+			_navigationService.NavigateTo (AppPages.DetailsPage);
+		}
+	}
+}
